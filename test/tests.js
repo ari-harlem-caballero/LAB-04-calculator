584745762a06feb07057c12167a94798cf6ef8fd
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2021-10-07 11:16:17 -0700

    started calucator test

--- test/tests.js
+++ test/tests.js
@@ -3,15 +3,20 @@
 
 const test = QUnit.test;
 
+function add(num1, num2) {
+    return num1 + num2;
+}
 // name your test by what it is testing
-test('time to test a function', (expect) => {
+test('adds two numbers', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 5;
+    const y = 3;
+    const expected = 8;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = add(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
