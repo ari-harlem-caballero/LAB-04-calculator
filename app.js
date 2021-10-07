3711ef761a1f8fde7d40da85d6c14a2a424023d0
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2021-10-07 12:06:50 -0700

    created calculator basis

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-// import functions
 
-// reference needed DOM elements
+import { add } from './calculations.js';
+
 
 
 // set event listeners 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
+
+console.log(addNumber1, addNumber2, addButton, addAnswer);
+
+addButton.addEventListener('click', () => {
+    const num1 = +addNumber1.value;
+    const num2 = +addNumber2.value;
+
+    const sum = add(num1, num2);
+
+    addAnswer.textContent = sum;
+}
+)
